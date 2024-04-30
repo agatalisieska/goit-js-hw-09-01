8c746ed5551b6516f885b8a5eab2fe46ce5767a6
agatalisieska <ag.pietrusinska@gmail.com>
2024-04-30 13:11:10 +0200

    zatrzymanie zmiany koloru

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,12 +2,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
+let intervalId;
 // funkcja do zmiany tła całej sekcji body - odwołuję się do 'backgroundColor' i uruchamiam funkcję do zmiany tła
 function changeBackgroundColor() {
+  if (!intervalId) {
+    // zmiana koloru co 1 sekundę;
+    intervalId = setInterval(changeBackgroundColor, 1000);
+  }
   document.body.style.backgroundColor = getRandomHexColor();
-  // zmiana koloru co 1 sekundę
-  setTimeout(changeBackgroundColor, 1000);
 }
 // dodaję operację do przycisku 'start' (nasłuchiwanie na kliknięcie) - po kliknięciu ma się wykonać funkcja
 document.querySelector('[data-start]').addEventListener('click', function () {
@@ -21,12 +23,10 @@ document.querySelector('[data-start]').addEventListener('click', function () {
 
 // dodaję operację do przycisku 'stop' (nasłuchiwanie na kliknięcie)
 document.querySelector('[data-stop]').addEventListener('click', function () {
+  clearInterval(intervalId);
+  intervalId = null;
   // po kliknięciu na 'stop' przycisk ma zostać wyłączony
   this.disabled = true;
   // po kliknięciu na 'stop', przyciak 'start' ma zostać włączony
   document.querySelector('[data-start]').disabled = false;
-  // to mi nie działa, nie wiem jak ustawić wyłączenie nasłuchiwania dla przycisku 'start'
-  document
-    .querySelector('[data-start]')
-    .removeEventListener('click', changeBackgroundColor);
 });
